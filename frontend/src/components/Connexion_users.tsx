b429875b612ac317ff33e8eee2edafd4e7b7103c
Damien82 <87046337+Damien82@users.noreply.github.com>
2025-07-23 13:43:19 +0100

    Update Connexion_users.tsx

--- frontend/src/components/Connexion_users.tsx
+++ frontend/src/components/Connexion_users.tsx
@@ -18,7 +18,7 @@ const LoginForm: React.FC = () => {
     setError(null);
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch("https://iai-vote.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ matricule, motDePasse: password }),
