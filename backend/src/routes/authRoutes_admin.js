29d545dd17184585d81a255a99b94b80725e6c21
mbida francois <charoma220@gmail.com>
2025-07-24 10:38:52 +0100

    modification du ficher app.js pour l'utilisation des fichier admins

--- backend/src/routes/authRoutes_admin.js
+++ backend/src/routes/authRoutes_admin.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { registerAdmin, loginAdmin } = require("../controllers/authController_admin");
 
 // Route inscription
-router.post("/register", registerAdmin);
+router.post("/registerAdmin", registerAdmin);
 
 // ✅ Route de connexion
-router.post("/login", loginAdmin);
+router.post("/loginAdmin", loginAdmin);
 
 module.exports = router;
