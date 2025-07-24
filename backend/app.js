1e7037f23da359360d06a597c7b752f9f1867fea
mbida francois <charoma220@gmail.com>
2025-07-24 14:19:39 +0100

    modification du ficher app.js pour l'utilisation des fichier admins

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://iai-vote-v12.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Connexion à la base `acces_users`
