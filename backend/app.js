32b6cabb6ee71c5b4b1a3e2797680891700f406a
mbida francois <charoma220@gmail.com>
2025-07-24 14:50:22 +0100

    modification du ficher app.js pour l'utilisation des fichier admins v2

--- backend/app.js
+++ backend/app.js
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.use("/api/auth", authRoutes);
-app.use("/api/authadmin", authRoutes_admin);
+app.use("/api/authAdmin", authRoutes_admin);
 
 // Lancement du serveur
 app.listen(PORT, () => {
