27c49f29cb17ea945cdfa2d0fcbee172f2564ddb
mbida francois <charoma220@gmail.com>
2025-07-25 13:50:08 +0100

    modification du ficher app.js

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -5,6 +5,7 @@ const Loader = () => {
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+      <p className="ml-5 font-bold">Chargement en cours...</p>
     </div>
   );
 };
