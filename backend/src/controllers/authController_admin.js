be78e79cba80232f9a1af4e1eb1b31f1d08f454d
mbida francois <charoma220@gmail.com>
2025-07-25 14:33:05 +0100

    modification du fichier routes

--- backend/src/controllers/authController_admin.js
+++ backend/src/controllers/authController_admin.js
@@ -75,3 +75,25 @@ exports.loginAdmin = async (req, res) => {
     res.status(500).json({ message: "Erreur serveur" });
   }
 };
+
+//== ROUTE:POST /api/verifyAdmin ====
+exports.verifyAdmin = async (req, res) => {
+  const { matricule } = req.body;
+  if (!matricule) return res.status(400).json({ message: "Matricule requis" });
+
+  const Admin = require("../models/Admins")(req.db_admin.registeredAdmins);
+
+  try {
+    const admin = await Admin.findOne({ matricule });
+
+    if (!admin) {
+      return res.status(403).json({ message: "Accès refusé" });
+    }
+
+    return res.status(200).json({ message: "Admin confirmé" });
+  } catch (err) {
+    console.error("Erreur de vérification admin :", err);
+    return res.status(500).json({ message: "Erreur serveur" });
+  }
+};
+
--- backend/src/routes/authRoutes_admin.js
+++ backend/src/routes/authRoutes_admin.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { registerAdmin, loginAdmin } = require("../controllers/authController_admin");
+const { registerAdmin, loginAdmin, verifyAdmin, } = require("../controllers/authController_admin");
 
 // Route inscription
 router.post("/registerAdmin", registerAdmin);
 
 // ✅ Route de connexion
 router.post("/loginAdmin", loginAdmin);
+
+// nouvelle route de vérification
+router.post("/verify-admin", verifyAdmin);
 
 module.exports = router;
