3edae3b422f123a8ef7befcd73c7f1c5dbfb01d1
mbida francois <charoma220@gmail.com>
2025-07-29 08:34:01 +0100

    modification du fichier routes

--- backend/src/models/Parti.js
+++ backend/src/models/Parti.js
@@ -1,15 +1,12 @@
+const mongoose = require('mongoose');
+
+const PartiSchema = new mongoose.Schema({
+  nom: { type: String, required: true, trim: true },
+  proprietaire: { type: String, required: true, trim: true },
+  imageUrl: { type: String, required: true },
+});
+
+// Cette fonction vérifie si le modèle existe déjà dans la connexion personnalisée
 module.exports = (db_partis) => {
-  if (db_partis.models.PartisInfos) {
-    return db_partis.models.PartisInfos;
-  }
-
-  const mongoose = require('mongoose');
-
-  const PartiSchema = new mongoose.Schema({
-    nom: { type: String, required: true, trim: true },
-    proprietaire: { type: String, required: true, trim: true },
-    imageUrl: { type: String, required: true },
-  });
-
-  return db_partis.model('PartiInfos', PartiSchema);
+  return db_partis.models.PartiInfos || db_partis.model('PartiInfos', PartiSchema);
 };
