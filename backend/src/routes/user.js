af6748a45789917690d3b8688a72adbd24998f10
mbida francois <charoma220@gmail.com>
2025-07-30 10:10:15 +0100

    modification du fichier routes

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -6,18 +6,19 @@ const User = require("../models/Admins"); // ou Users si c'est pas pour les admi
 
 router.get("/me", verifyToken, async (req, res) => {
   try {
-    const matricule = req.admin?.matricule; // si middleware = req.admin = decoded
+    console.log("req.admin:", req.admin);
 
+    const matricule = req.admin?.matricule;
     if (!matricule) {
       return res.status(400).json({ message: "Matricule manquant dans le token" });
     }
 
-    const user = await User.findOne({ matricule }).select("-password");
-    if (!user) {
-      return res.status(404).json({ message: "Utilisateur non trouvé" });
+    const admin = await Admin.findOne({ matricule }).select("-password");
+    if (!admin) {
+      return res.status(404).json({ message: "Admin non trouvé" });
     }
 
-    res.json(user);
+    res.json(admin);
   } catch (error) {
     console.error("Erreur GET /me :", error);
     res.status(500).json({ message: "Erreur serveur" });
