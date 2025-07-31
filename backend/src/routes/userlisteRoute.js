214df5a54a634bdd795a7dcfbab11b7869b2d01c
mbida francois <charoma220@gmail.com>
2025-07-31 12:48:26 +0100

    ajout des des fichiers et api pour la gestion et affichage des users

--- backend/src/routes/userlisteRoute.js
+++ backend/src/routes/userlisteRoute.js
@@ -1,4 +1,4 @@
-import express from "express";
+const express = require("express");
 import { ajouterUser, afficherUsers } from "../controllers/simpleuserController.js";
 
 const router = express.Router();
