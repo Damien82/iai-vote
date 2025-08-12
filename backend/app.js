f76f0f48dd1ddae2a131013b16aad8c6e40b9ace
mbida francois <charoma220@gmail.com>
2025-08-12 16:42:46 +0100

    ajout des des fichiers et api pour tout les composant superadmin

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ const verifyUserRoute = require("./src/routes/userRoutes");
 const partisRoutes = require('./src/routes/partiRoutes');
 
 const authMiddleware = require("./src/middlewares/authMiddleware_supadmin");
-const { changePassword } = require("./src/controllers/authController_superadmin");
+const { changePassword } = require("./src/controllers/authController_admin");
 
 const userRoutes = require("./src/routes/user");
 const userlisteRoutes = require( "./src/routes/userlisteRoute");
@@ -108,7 +108,7 @@ app.use('/api/admins', adminlisteRoutes);
 app.use('/api/admins', superAdminRoutes2);
 
 
-app.post('/api', authMiddleware, changePassword);
+app.put('/api', authMiddleware, changePassword);
 
 // ⛔ Gestion des erreurs Multer
 const errorHandler = require("./src/middlewares/errorHandler");
