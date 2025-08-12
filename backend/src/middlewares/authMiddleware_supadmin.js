f0c4b4298a8c3dd333897c1e37c42051b2e4c431
mbida francois <charoma220@gmail.com>
2025-08-12 17:22:05 +0100

    ajout des des fichiers et api pour tout les composant superadmin

--- backend/src/middlewares/authMiddleware_supadmin.js
+++ backend/src/middlewares/authMiddleware_supadmin.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET || 'secret';
 
 const authMiddleware = (req, res, next) => {
+console.log('authMiddleware appelé, token:', req.headers.authorization);
   const authHeader = req.headers.authorization;
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({ message: 'Token manquant' });
