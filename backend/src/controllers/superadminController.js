8f106eb4f6bb6342098ee3e9a7f4579e52d7d63c
mbida francois <charoma220@gmail.com>
2025-08-13 16:56:08 +0100

    ajout des des fichiers et api pour tout les composant superadmin

--- backend/src/controllers/superadminController.js
+++ backend/src/controllers/superadminController.js
@@ -1,19 +1,23 @@
 // controllers/userController.js
 const jwt = require('jsonwebtoken');
-const User = require('../models/SuperAdmin'); // modèle user
 
 const getProfile = async (req, res) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) return res.status(401).json({ error: "Token manquant" });
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findOne({ matricule: decoded.matricule }).select("-password");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET || "tonSecretJwtIci");
+
+    const SuperAdmin = require('../models/SuperAdmin')(req.db_superadmin.registeredSuperAdmins);
+    const user = await SuperAdmin.findOne({ matricule: decoded.matricule }).select("-password");
+
     if (!user) return res.status(404).json({ error: "Utilisateur non trouvé" });
 
     res.status(200).json(user);
   } catch (err) {
+    console.error("Erreur getProfile:", err);
     res.status(500).json({ error: "Erreur serveur" });
   }
 };
+
 module.exports = { getProfile };
