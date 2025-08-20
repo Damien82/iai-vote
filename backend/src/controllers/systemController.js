b3f58a28e7a81e3edd5e3d0d5520a72292bc0588
mbida francois <charoma220@gmail.com>
2025-08-20 16:17:43 +0100

    ajout des des fichiers et api pour le blocage de la page vote

--- backend/src/controllers/systemController.js
+++ backend/src/controllers/systemController.js
@@ -1,5 +1,4 @@
 const mainConnection = require("../config/db_status"); // ton fichier de connexion
-const { mainConnection } = connectDB();
 const System = require("../models/SystemState")(mainConnection);
 
 const getSystemState = async (req, res) => {
