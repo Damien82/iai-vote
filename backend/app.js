8b0e918d255be4f9fff75469f44d2e2542355c20
mbida francois <charoma220@gmail.com>
2025-08-26 11:03:53 +0100

    ajout des des fichiers et api pour tout les composant pagestatus

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,6 @@ console.log("DB_URI_REGISTERED_SUPERADMINS =", process.env.DB_URI_REGISTERED_SUP
 const express = require("express");
 const connectDB = require("./src/config/db");
 const connectDB2 = require("./src/config/db_admin");
-const mainConnection = require("./src/config/db_status");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const authRoutes = require("./src/routes/authRoutes");
@@ -107,7 +106,6 @@ app.use((req, res, next) => {
 
 connectDB();
 connectDB2();
-mainConnection();
 
 
 // Routes
