087ba10bccd0aa2a7e927bd88a51415bcc612d00
mbida francois <charoma220@gmail.com>
2025-08-26 10:24:28 +0100

    ajout des des fichiers et api pour tout les composant pagestatus

--- backend/src/models/SystemState.js
+++ backend/src/models/SystemState.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
+const mainConnection = require("../config/db_status");
 
 const SystemSchema = new mongoose.Schema({
   isActive: { type: Boolean, default: false },
 });
 
 // Fonction qui retourne le modèle avec la bonne connexion
-module.exports = (db_status) => {
-  if (!db_status) {
-    throw new Error("❌ db_status n’a pas été fourni à SystemState.js");
-  }
-  return db_status.models.status || db_status.model("status", SystemSchema);
-};
+module.exports =
+  mainConnection.models.SystemState ||
+  mainConnection.model("status", SystemSchema);
