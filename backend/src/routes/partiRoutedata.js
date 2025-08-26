cda702efca143b31ad4592c8f0e67f801ad04931
mbida francois <charoma220@gmail.com>
2025-08-26 13:40:33 +0100

    ajout des des fichiers et api pour tout les composant pagestatus

--- backend/src/routes/partiRoutedata.js
+++ backend/src/routes/partiRoutedata.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const partiesController = require("../controllers/partiesController"); // <--- bien vérifier le chemin
+const partiesController = require("../controllers/partiesdata"); // <--- bien vérifier le chemin
 
 // Route pour récupérer tous les partis
 router.get("/getAllPartis", partiesController.getAllPartis);
